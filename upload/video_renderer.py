4c7acafa9d6d22930a454d710222c09b759d5989
Muhammad Nashif Farid <134785469+nashiffrd@users.noreply.github.com>
2026-02-11 22:08:28 +0700

    Update video_renderer.py

--- upload/video_renderer.py
+++ upload/video_renderer.py
@@ -15,7 +15,7 @@ def create_motility_video(video_path, tracks_df, motility_results):
     
     # Setup Video Writer
     temp_out = tempfile.NamedTemporaryFile(delete=False, suffix='.mp4')
-    fourcc = cv2.VideoWriter_fourcc(*'H264')
+    fourcc = cv2.VideoWriter_fourcc(*'avc1')
     out = cv2.VideoWriter(temp_out.name, fourcc, fps, (width, height))
     
     # Warna: BGR (OpenCV menggunakan BGR bukan RGB)
